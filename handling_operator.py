8e3b6d1854246990f5c0ceafa28f400b32933d3c
lionalio <lionalio87@gmail.com>
2023-06-28 21:46:19 +0700

    update new handling for code and newer test cases

--- handling_operator.py
+++ handling_operator.py
@@ -1,8 +1,17 @@
+import re
+
 class HandlingOperator():
     '''
     Class for handling pricelist of all operators
     Also handling dial number to find the operator providing cheapest
     price for it
+
+    The format should not affect the dialed number, they should be the same: 
+    (123)456-7890
+    123-456-7890
+    1234567890 (difficult to read or remember, though...)
+
+    So whatever the format is, just convert them to pure numbers!
 
     Attributes:
         pricelists (dict): dictionary of operator associated with its own pricelist
@@ -13,6 +22,18 @@ class HandlingOperator():
         self.pricelists = {}
         self._dial_number = ''
         self.dial_prefixes = []
+
+    def formatter(self, num_str):
+        '''
+        Return any format of prefixes, dialed number into string containing number only
+
+        Args: 
+            num_str (str): string of number / prefix
+
+        Returns:
+            string of formatted number
+        '''
+        return re.sub(r'[^0-9]', '', num_str)
 
     @property
     def dial_number(self):
@@ -26,7 +47,7 @@ class HandlingOperator():
         """
         Set dial number when receiving new dial number
         """
-        self._dial_number = str(dial_)
+        self._dial_number = self.formatter(str(dial_))
         self.dial_prefixes = self.get_all_dial_prefixes()
 
     def get_all_dial_prefixes(self):
@@ -41,8 +62,28 @@ class HandlingOperator():
         """
         ls = [self._dial_number[:i+1] for i in range(len(self._dial_number)-1)]
         ls.reverse()
-        
+
         return ls
+
+    def load_pricelist_to_dict(self, pricelist):
+        '''
+        Load a file containing pricelist into dictionary
+        
+        Args: 
+            pricelist (str): file name
+        
+        Returns:
+            dictionary of key of prefix and value of price
+        '''
+
+        d = {}
+        with open(pricelist) as f:
+            for line in f:
+                key, val = line.split()
+                key = self.formatter(str(key))
+                d[key] = float(val)
+        
+        return d
 
     def add_pricelist(self, pricelist):
         '''
@@ -68,27 +109,11 @@ class HandlingOperator():
         Returns:
             None
         '''
-        for list in pricelists:
-            self.add_pricelist(list)
-
-    def load_pricelist_to_dict(self, pricelist):
-        '''
-        Load a file containing pricelist into dictionary
-        
-        Args: 
-            pricelist (str): file name
-        
-        Returns:
-            dictionary of key of prefix and value of price
-        '''
-
-        d = {}
-        with open(pricelist) as f:
-            for line in f:
-                key, val = line.split()
-                d[str(key)] = float(val)
-        
-        return d
+        if isinstance(pricelists, str):
+            self.add_pricelist(pricelists)
+        elif isinstance(pricelists, list):
+            for ls in pricelists:
+                self.add_pricelist(ls)
 
     def find_cheapest_operator(self):
         '''
@@ -98,7 +123,7 @@ class HandlingOperator():
             None
 
         Returns:
-            name of operator providing the cheapest price for that dial number
+            List of names of operator providing the cheapest price for that dial number
         '''
         prices = {}
         for operator, pricelist in self.pricelists.items():
@@ -108,10 +133,12 @@ class HandlingOperator():
                     break
 
         if prices:
-            return min(prices, key=prices.get)
+            min_price = min(prices.values())
+            ls_min_prices = [k for k, v in prices.items() if v == min_price]
+            return ls_min_prices
         else:
-            print('Warning: No operator with minimum price exist with number ', self._dial_number)
-            return ''
+            print('Warning: No operator with minimum price exist with number', self._dial_number)
+            return []
 
 
 
--- unit_test.py
+++ unit_test.py
@@ -3,21 +3,36 @@ from handling_operator import HandlingOperator
 
 
 class TestHandlingOperator(unittest.TestCase):
+    '''
+    Operator A and B file should contain normal prefix with numbers only
+    Operator C is a little bit exotic, with some minus and parentheses as separators (for easy reading)
+    '''
     def test_normal_case(self):
+        print('\n=== Running a very normal test case using two operator ===')
         handling = HandlingOperator()
         pricelists = ['operator_A.txt', 'operator_B.txt']
         dial = '4673212345'
         handling.add_all_pricelists(pricelists)
         handling.dial_number = dial
-        self.assertEqual(handling.find_cheapest_operator(), 'operator_B.txt', "Incorrect operator")
+        self.assertEqual(handling.find_cheapest_operator(), ['operator_B.txt'], "Incorrect operator")
         
     def test_none_match(self):
+        print('\n=== Running test case without any pricelist exist ===')
         handling = HandlingOperator()
         pricelists = ['operator_A.txt', 'operator_B.txt']
         dial = '3214567890'
         handling.add_all_pricelists(pricelists)
         handling.dial_number = dial
-        self.assertEqual(handling.find_cheapest_operator(), '', "Operator should not exist, so return empty!")
+        self.assertEqual(handling.find_cheapest_operator(), [], "Operator should not exist, so return empty!")
+
+    def test_multiple_min_price(self):
+        print('\n=== Running test case with more than one operator has minimum prices ===')
+        handling = HandlingOperator()
+        pricelists = ['operator_A.txt', 'operator_B.txt', 'operator_C.txt']
+        dial = '4673212345'
+        handling.add_all_pricelists(pricelists)
+        handling.dial_number = dial
+        self.assertEqual(handling.find_cheapest_operator(), ['operator_B.txt', 'operator_C.txt'], "Incorrect list of operator")
 
 
 if __name__ == '__main__':
